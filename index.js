6a5d7ad8dbe81e5331c3c3b3dfa5c95b82c4c32a
Dariusz Kobuszewski <quack.code.dk@gmail.com>
2018-09-08 11:01:22 +0200

    backend/index.js: socket.io dropped in (need refactors in future)

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
 const express = require("express");
 const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const SonyCamera = require("sony-camera");
 
 const cam = new SonyCamera();
 
-cam.on("update", function(param, data) {
-    console.log("updated: " + param  + " = ", data);
+cam.on("update", function (param, data) {
+    console.log("updated: " + param + " = ", data);
 });
 cam.connect((...args) => {
     console.log(cam.availableApiList);
     console.log(cam.params);
     console.log("Connected to camera!");
     console.log(args);
+    io.emit("camera-connected", cam);
+    cam.startViewfinder()
+});
+cam.on("disconnected", () => {
+    console.log("DISCONNECTED...")
+    io.emit("camera-disconnected", cam);
 });
 
-app.use(express.static('public'))
+cam.on("liveviewJpeg", (imgBuffer) => {
+    io.emit("camera-liveview", imgBuffer);
+});
 
-app.listen(6969);
+io.on("connection", function (socket) {
+    console.log("IO: a user connected");
+    if (cam.connected)
+        socket.emit("camera-connected", cam);
+    socket.on("camera-reconnect", function (socket) {
+        console.log("Trying reconnection on client demand...")
+        cam.connect((...args) => {
+            console.log(cam.availableApiList);
+            console.log(cam.params);
+            console.log("Connected to camera!");
+            console.log(args);
+            io.emit("camera-connected", cam);
+            cam.startViewfinder()
+        });
+    });
+});
+
+
+
+app.use(express.static("dist"));
+
+server.listen(6969);
 console.log("Sony Camera API Task builder run at 6969 port");
