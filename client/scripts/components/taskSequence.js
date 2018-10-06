9d07ff102d30e74646eba5a2fecac8dbf03f9863
Dariusz Kobuszewski <quack.code.dk@gmail.com>
2018-10-06 21:25:06 +0200

    taskSequence: saving & starting, putting task to Edit too

--- client/scripts/components/taskSequence.js
+++ client/scripts/components/taskSequence.js
@@ -1,10 +1,21 @@
 import taskManager from "../logic/TaskManager";
+import codeEditor from "../components/codeEditor";
 import * as rivets from "rivets";
 
 let taskSeqModel = {
+    codeEditor,
     taskManager,
-    editTask: function (evt, task) {
-        console.log(`edit task: `, task);
+    editTask: function (evt, ctx) {
+        console.log(`edit task: `, ctx.task);
+        codeEditor.setCurrentTask(ctx.task);
+    },
+    startSequence: function (evt, ctx) {
+        console.log(`running task sequence`);
+        return this.taskManager.runTasks();
+    },
+    saveSequence: function (evt, ctx) {
+        console.log(`save task sequence`);
+        return this.taskManager.saveTasks();
     }
 };
 
