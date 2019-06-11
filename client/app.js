a97f2b503cb88b0224f2542f244f45247198dc78
Darek Kobuszewski <quack.code.dk@gmail.com>
2019-06-12 00:34:04 +0200

    app.js: most of the script commented

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 import "./assets/styles/style.scss";
-import "./scripts/components/liveView";
-import "./scripts/components/taskManager";
-import "./scripts/components/splitter";
-import "./scripts/components/taskSequence";
-
+// import "./scripts/components/CameraLiveView";
+// import "./scripts/components/taskManager";
+// import "./scripts/components/splitter";
+// import "./scripts/components/taskSequence";
+import "./scripts/components/App"
