43b6a2961a56941072e804c3bed4a5e81618ceea
Darek Kobuszewski <quack.code.dk@gmail.com>
2019-07-22 00:57:47 +0200

    TasksStrip component for displaying all of the tasks

---
+++ client/scripts/components/TasksStrip/index.js
@@ -0,0 +1,29 @@
+import {TasksContext} from "../rightPanel/TaskEditor/TasksContext";
+import React, {useContext, useState} from "react";
+
+
+const TasksStrip = ({width}) => {
+  const {tasks = [], removeTask, setCurrentTask} = useContext(TasksContext);
+  const [drawerOpened, setDrawerOpened] = useState(false);
+
+  function renderNoTasksMsg() {
+    return <div>Seems like you don't have any task sequence to run at the camera by now.</div>
+  }
+  function renderTasksStripHandle() {
+    return <div className="tasks-handle-wrapper" ><div className="tasks-handle"><i className="material-icons">expand_less</i></div></div>
+  }
+  const openDrawer = () => setDrawerOpened(true);
+  const closeDrawer = () => setDrawerOpened(false);
+  const getTaskStripClasses = () => `tasks-strip ${drawerOpened ? "drawed" : ""}`;
+
+  return <div className={getTaskStripClasses()} style={{width}}
+              onMouseOver={openDrawer} onMouseOut={closeDrawer}>
+    {renderTasksStripHandle()}
+    <div className="task-container">
+      {tasks.map(task => <div className={`task ${task.active ? "active" : ""}`}>{task.name}</div> )}
+      {!tasks.length && renderNoTasksMsg()}
+    </div>
+  </div>
+};
+
+export default TasksStrip;
