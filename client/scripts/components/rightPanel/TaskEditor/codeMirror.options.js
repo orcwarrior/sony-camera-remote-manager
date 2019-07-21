8a97f3f42e25779e55326e2b4c407ef20cf50661
Darek Kobuszewski <quack.code.dk@gmail.com>
2019-07-22 00:54:46 +0200

    codeMirror.options: defautl params for CM instance

---
+++ client/scripts/components/rightPanel/TaskEditor/codeMirror.options.js
@@ -0,0 +1,15 @@
+export const options =  {
+    lineNumbers: true,
+    theme: "darcula",
+    mode: "javascript",
+    tabSize: 2,
+    keyMap: "sublime",
+    autoCloseBrackets: true,
+    matchBrackets: true,
+    indentUnit: 1,
+    gutters: ["exec-cur-line"],
+
+    value: `// No task loaded, treat this code as Scratchpad ;-)
+await CameraAPI.captureAndSave();
+`
+};
