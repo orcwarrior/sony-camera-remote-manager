7d450a754bec02dc7461c8b1e93abebce2286a10
Dariusz Kobuszewski <quack.code.dk@gmail.com>
2019-08-15 23:44:41 +0200

    Tab.Task: added TaskManager funcs

--- client/scripts/components/rightPanel/Tab.Task.js
+++ client/scripts/components/rightPanel/Tab.Task.js
@@ -1,20 +1,29 @@
 import React, {useContext} from "react";
 import TaskEditor from "./TaskEditor/TaskEditor";
-import CodeMirrorTern from "./TaskEditor/CodeMirrorTern";
-import {options} from "./TaskEditor/codeMirror.options";
-import {EditorCodeContext} from "./TaskEditor/EditorCodeContext";
-import TaskManager from "../../logic/TaskManager"
+import {EditorCodeContext} from "../../logic/contexts/EditorCodeContext";
+import {TasksContext} from "../../logic/contexts/TasksContext";
+import TaskManagerInitializer from "../../logic/TaskManager"
+
 export default function TabTask(props) {
-    const {currentTask} = useContext(EditorCodeContext);
+    const EditorCodeCtx = useContext(EditorCodeContext);
+    const TasksCtx = useContext(TasksContext);
+    const taskManager = TaskManagerInitializer();
+
     function testCode() {
+        const {editorCode} = EditorCodeCtx;
+        const currentTask = {name: "TEST-TASK", code: editorCode};
         console.log(`currentTask: `, currentTask);
-        TaskManager.runTask({name: "TEST-TASK", code: currentTask})
+        taskManager.runTask(currentTask)
+    }
+    function createNewTask() {
+        const {editorCode} = EditorCodeCtx;
+        TasksCtx.createTask({name: null, code: editorCode, active: true})
     }
 
     return <section className="task tab-content">
         <div className="btns-wrapper">
             <a className="test-code-btn btn" onClick={testCode}><span>TEST CODE</span><i className="material-icons">play_arrow</i></a>
-            <a className="create-task-btn btn"><span>CREATE NEW TASK</span><i
+            <a className="create-task-btn btn" onClick={createNewTask}><span>CREATE NEW TASK</span><i
                 className="material-icons">add_circle_outline</i></a>
         </div>
         <div className="editor-wrapper">
