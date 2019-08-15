08fb939bf89d2d9d0eda795fae8c583c58600bd0
Dariusz Kobuszewski <quack.code.dk@gmail.com>
2019-08-15 23:25:25 +0200

    LiveviewImg: use withSocketIO

--- client/scripts/components/LiveviewImg.js
+++ client/scripts/components/LiveviewImg.js
@@ -1,37 +1,40 @@
 import {useEffect, useState} from "react";
-import {getSocket} from "../cameraSocketIOHandler";
 import React from "react";
+import withSocketIO from "./hoc/withSocketIO";
+import _ from "lodash";
 
 
 let lastImageSetTS = 0;
+
 function buildLiveimageListener(setLiveviewUrl, throttleMs = 100) {
-    return function createNewLiveviewImg(buffer) {
-        const blob = new Blob([buffer], {type: "image/jpeg"});
-        const urlCreator = window.URL || window.webkitURL;
-        const imgUrl = urlCreator.createObjectURL(blob);
+  return function createNewLiveviewImg(buffer) {
+    const blob = new Blob([buffer], {type: "image/jpeg"});
+    const urlCreator = window.URL || window.webkitURL;
+    const imgUrl = urlCreator.createObjectURL(blob);
 
-        if (new Date().valueOf() - lastImageSetTS > throttleMs) {
-            setLiveviewUrl(imgUrl);
-            lastImageSetTS = new Date().valueOf();
-        }
+    if (new Date().valueOf() - lastImageSetTS > throttleMs) {
+      setLiveviewUrl(imgUrl);
+      lastImageSetTS = new Date().valueOf();
     }
+  };
 }
 
-function LiveviewImg(props) {
+const LiveviewImg = withSocketIO(function LiveviewImg({socket}) {
 
-    const [liveviewUrl, setLiveviewUrl] = useState();
+  const [liveviewUrl, setLiveviewUrl] = useState();
 
-    // const throttledLiveviewImgGeneration = _.throttle(createNewLiveviewImg, 1200);
+  // const throttledLiveviewImgGeneration = _.throttle(createNewLiveviewImg, 1200);
 
-    useEffect(() => {
-        const eventCb = buildLiveimageListener(setLiveviewUrl, 30);
-        getSocket().on("camera-liveview", eventCb);
-        return function cleanup() {
-            getSocket().off("camera-liveview",eventCb);
-        }
-    });
+  useEffect(() => {
+    if (_.isFunction(socket)) socket = socket();
+    const eventCb = buildLiveimageListener(setLiveviewUrl, 30);
+    socket.on("camera-liveview", eventCb);
+    return function cleanup() {
+      socket.off("camera-liveview", eventCb);
+    };
+  });
 
-    return <div className="liveimg-wrapper"><img src={liveviewUrl}/></div>
-}
+  return <div className="liveimg-wrapper"><img src={liveviewUrl} alt="camera viewport"/></div>;
+});
 
-export {LiveviewImg}
+export {LiveviewImg};
