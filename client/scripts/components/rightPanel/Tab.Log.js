817752d87ef846b1b40c125b5c154c16eea4bcba
Dariusz Kobuszewski <quack.code.dk@gmail.com>
2019-08-15 23:44:23 +0200

    Tab.Log: added logs

--- client/scripts/components/rightPanel/Tab.Log.js
+++ client/scripts/components/rightPanel/Tab.Log.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, {useContext} from "react";
+import {LogsContext} from "../../logic/contexts/LogsContext";
 
 export default function TabLog(props) {
-    
-    return <section className="log tab-content" rv-show="activeTab | eq 'Log'">
-        <div className="tab-inner-container logs-container" rv-html="logs | joinMarkup">
-
-        </div>
-        <div className="btns-wrapper justify-end">
-            <a className="test-code-btn btn" rv-on-click="clearLogs">
-                <span>CLEAR</span><i className="material-icons">delete</i></a>
-        </div>
-    </section>
+  const {logs, clearLogs} = useContext(LogsContext);
+  return <section className="log tab-content">
+    <div className="tab-inner-container logs-container">
+      {logs.map((log) => <div className="log" dangerouslySetInnerHTML={{__html: log}} />)}
+    </div>
+    <div className="btns-wrapper justify-end">
+      <a className="test-code-btn btn" onClick={clearLogs}>
+        <span>CLEAR</span><i className="material-icons">delete</i></a>
+    </div>
+  </section>;
 };
